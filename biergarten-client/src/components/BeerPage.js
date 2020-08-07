e672285c70338c029a5cccb726ec1a4fd17de360
kevinneyer <kevinneyer@gmail.com>
2020-08-07 11:27:43 -0400

    fetch and render using hooks

--- biergarten-client/src/components/BeerPage.js
+++ biergarten-client/src/components/BeerPage.js
@@ -1,29 +1,43 @@
 import React from 'react'
 import Review from './Review'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const BeerPage = (props) => {
 
     let beerId = props.match.params.id 
-    let beerToDisplay = props.beers.find(beer => beer.id === parseInt(beerId))
+    // let showBeer = props.beers.find(beer => beer.id === parseInt(beerId))
 
-    const [likes, setLikes] = useState('')
-    console.log(beerToDisplay)
+    const [showBeer, setShowBeer] = useState([])
+
+  
+    const fetchBeer = () => {
+       fetch(`http://localhost:3001/api/v1/beers/${beerId}`)
+      .then(res => res.json())
+      .then(data =>{
+        setShowBeer(data)
+      }) 
+    }   
+    
+    useEffect (() => {
+       fetchBeer()
+    }, [])
+
+    console.log(showBeer)
     return (
         <div>
           <div className='review-div'>
             <Review />
           </div>
-          {beerToDisplay ? 
+          {showBeer ? 
             (<div className='beer-page'>
-                <h1>{beerToDisplay.name}</h1>
-                <h3>by {beerToDisplay.brewery}</h3>
-                <img className='show-image' src={beerToDisplay.img_url} alt={beerToDisplay.name} />
-                <p>Likes {beerToDisplay.likes} <button>like</button></p>
-                <p>ABV: {beerToDisplay.abv}%</p>
-                <p>Style: {beerToDisplay.style}</p>
-                <p>Tasting Notes: {beerToDisplay.tasting_notes}</p>
-                <p>Recommended For: {beerToDisplay.recommended_drinking}</p>
+                <h1>{showBeer.name}</h1>
+                <h3>by {showBeer.brewery}</h3>
+                <img className='show-image' src={showBeer.img_url} alt={showBeer.name} />
+                <p>Likes {showBeer.likes} <button>like</button></p>
+                <p>ABV: {showBeer.abv}%</p>
+                <p>Style: {showBeer.style}</p>
+                <p>Tasting Notes: {showBeer.tasting_notes}</p>
+                <p>Recommended For: {showBeer.recommended_drinking}</p>
             </div>)
             : (
                 <div>Loading</div>
