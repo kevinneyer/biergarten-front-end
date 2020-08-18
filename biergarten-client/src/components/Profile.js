e0dceab407327a0fed7164413a4103cfeb3cab0a
kevinneyer <kevinneyer@gmail.com>
2020-08-18 16:36:00 -0400

    profile almost finished

--- biergarten-client/src/components/Profile.js
+++ biergarten-client/src/components/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import EditProfile from './EditProfile'
 import Favorites from './Favorites'
-import { Divider, Grid, Comment, Header, Segment, Image, Card } from 'semantic-ui-react'
+import {  Grid, Comment, Header, Segment, Image, Card, Feed , Icon} from 'semantic-ui-react'
 
 class Profile extends React.Component{
 
@@ -21,6 +21,7 @@ class Profile extends React.Component{
         <>
         <h1 className='profile-header'>{this.props.currentUser ? this.props.currentUser.username + '\'s ' + 'Profile Page' : 'Profile Page'}</h1>
           <Segment>
+         
           <Grid columns={3} relaxed='very'>
             <Grid.Column width={6}>
               <Segment raised>
@@ -31,12 +32,22 @@ class Profile extends React.Component{
                   <Card.Content >
                   {this.props.currentUser ? <Card.Header >{this.props.currentUser.username}</Card.Header> : "No User. Please Login"  }
                   </Card.Content>
+                  <Card.Content extra>
+                    {this.props.currentUser ? 
+                    <>
+                    <Icon name='user' />
+                    <span>{this.props.currentUser.followers.length} followers</span>
+                    </>
+                    :
+                    null
+                    }
+                  </Card.Content>
                   {/* </div> */}
                 {/* </div> */}
                 </Card>
                 </Segment>
-                <br/>
-                <div>
+                
+                {/* <div> */}
 
                 <Header as='h3' dividing>
                 My Reviews
@@ -60,47 +71,75 @@ class Profile extends React.Component{
               </Segment>
               {/* <button onClick={this.formHandler}>Edit Information</button> */}
               {/* </div><br/> */}
-
+{/* 
               <div>
                   {this.state.form ? <EditProfile currentUser={this.props.currentUser} /> : null}
-              </div>
-              </div>
+              </div> */}
+              {/* </div> */}
               </Grid.Column>
-              <Grid.Column width={6}>
+
+              <Grid.Column >
                 <Segment>
               {this.props.currentUser ? <Favorites currentUser={this.props.currentUser} /> : null}
               </Segment>
               </Grid.Column>
-              <Grid.Column width={4}>
+
+              {/* <Grid.Column width={4}>
                 <Segment raised>
                   <div>
                 {this.props.currentUser ? 
                 <>
                 <p>Following {this.props.currentUser.followeds.length} Users:</p>
-                <p>{this.props.currentUser.followeds.map(followed => 
+                {this.props.currentUser.followeds.map(followed => 
                   // <li>{followed.username} <button onClick={() => this.props.history.push(`users/${followed.id}`)}>View Profile</button></li>)} </p>
                   <Comment>
                     <Image src={followed.image} size='small'/>
                   <Comment.Content>
                     <Comment.Author><a href={`/users/${followed.id}`}>{followed.username}</a></Comment.Author>
                   </Comment.Content>
-                </Comment>)}</p>
+                </Comment>)}
                 <p> {this.props.currentUser.followers.length} Followers: </p>
-                <p>{this.props.currentUser.followers.map(follower => 
+                {this.props.currentUser.followers.map(follower => 
                   // <li>{follower.username} <button onClick={() => this.props.history.push(`users/${follower.id}`)}>View Profile</button></li>)} </p>
                   <Comment>
                     <Image src={follower.image} size='small'/>
                   <Comment.Content>
                     <Comment.Author><a href={`/users/${follower.id}`}>{follower.username}</a></Comment.Author>
                   </Comment.Content>
-                </Comment>)}</p>
+                </Comment>)}
                 </>
                 :
                 null
               }
               </div>
               </Segment>
-              </Grid.Column>
+              </Grid.Column> */}
+               <Grid.Column width={4}>
+               <Header as='h3'>Following</Header>
+              <Feed>
+                
+                {this.props.currentUser ? 
+                this.props.currentUser.followeds.map(
+                  followed =>
+                  <Feed.Event>
+                    <Feed.Label>
+                      <Image src={followed.image} size='mini'/>
+                    </Feed.Label>
+                   <Feed.Content>
+                      <Feed.Summary>
+                         <Feed.User href={`/users/${followed.id}`}>{followed.username}</Feed.User>
+                      </Feed.Summary>
+                    </Feed.Content>
+                  </Feed.Event>
+                )
+                 :
+                 null
+                }
+               
+
+              </Feed>
+              
+              </Grid.Column> 
           </Grid>
 
           {/* <Divider vertical></Divider> */}
